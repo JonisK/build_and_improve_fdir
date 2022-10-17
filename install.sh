4f4d48b7c5c1f306c4bacb56f0f66a3ecb8de6e1
Jonis Kiesbye <j.kiesbye@tum.de>
2022-10-17 16:58:39 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
 sudo apt update
-sudo apt install python3-pip openjdk-18-jdk
-pip3 install pydot networkx tqdm numpy
+sudo apt install git python3.8 python3-pip openjdk-17-jdk
+pip3 install xdot pydot networkx tqdm numpy
 
-cd prism
+# Download and install PRISM model checker
+git clone https://github.com/prismmodelchecker/prism.git
+cd prism/prism
+make
 ./install.sh
-
 
