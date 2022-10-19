eced27800eb40f11544134d4e6ff59b5d5ba66a4
JonisK <j.kiesbye@tum.de>
2022-10-19 11:38:07 +0200

    Add dtcontrol to install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install required software
 sudo apt update
 sudo apt install git python3.8 python3-pip openjdk-17-jdk
-pip3 install xdot pydot networkx tqdm numpy
+pip3 install xdot pydot networkx tqdm numpy dtcontrol
 
 # Download and install PRISM model checker
 git clone https://github.com/prismmodelchecker/prism.git
