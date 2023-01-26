93daaef36f3af3714a01b12555a847de9d9b0f13
Jonis Kiesbye <j.kiesbye@tum.de>
2023-01-26 19:01:56 +0100

    Add sensitivity analysis

---
+++ src/graph_analysis/sensitivity_analysis.py
@@ -0,0 +1,111 @@
+import numpy as np
+from graph_analysis.graph_analysis import get_node_id, get_fault_probability
+import pandas as pd
+
+
+def get_mode_gradients(G, equipment_fault_probabilities, mode_costs):
+    # row_index_to_equipment_name = {key: index for index, key in enumerate(equipment_fault_probabilities)}
+    row_index_to_equipment_name = {index: key for index, key in enumerate(equipment_fault_probabilities)}
+    column_index_to_mode_name = {index: key for index, key in enumerate(mode_costs)}
+
+    mode_gradients = np.zeros([len(equipment_fault_probabilities), len(mode_costs)])
+    for row_index, row in enumerate(mode_gradients):
+        for column_index, element in enumerate(row):
+            node_id = get_node_id(G, column_index_to_mode_name[column_index])
+            # node_id = get_node_id(G, row_index_to_equipment_name[row_index])
+            modified_fault_probabilities = equipment_fault_probabilities.copy()
+            modified_fault_probabilities[row_index_to_equipment_name[row_index]] *= 2
+            gradient = get_fault_probability(G, node_id, modified_fault_probabilities) - get_fault_probability(G,
+                                                                                                               node_id,
+                                                                                                               equipment_fault_probabilities)
+            mode_gradients[row_index, column_index] = gradient
+            # increased_mode_cost[row_index, column_index] = get_fault_probability(G, node_id, modified_fault_probabilities)
+    return mode_gradients
+
+def get_sensitivity_analysis(G, equipment_fault_probabilities, mode_costs):
+    mode_gradients = get_mode_gradients(G, equipment_fault_probabilities, mode_costs)
+    row_index_to_equipment_name = {index: key for index, key in enumerate(equipment_fault_probabilities)}
+    column_index_to_mode_name = {index: key for index, key in enumerate(mode_costs)}
+
+    message = "Sensitivity matrix\n------------------\n"
+    with pd.option_context('display.max_rows', 100, 'display.max_columns', 20):  # more options can be specified also
+        message += str(pd.DataFrame(mode_gradients,
+                                   columns=mode_costs.keys(),
+                                   index=equipment_fault_probabilities.keys()))
+        message += "\n\n"
+
+    for index, column in enumerate(mode_gradients.T):
+        message += f"Mode {column_index_to_mode_name[index]} is most sensitive to the component {row_index_to_equipment_name[column.argmax()]}\n"
+    message += "\n"
+
+    for index, row in enumerate(mode_gradients):
+        message += f"Component {row_index_to_equipment_name[index]} has most effect on mode {column_index_to_mode_name[row.argmax()]}\n"
+
+    return message
+
+
+def get_uncertainty_interval(G, equipment_fault_probabilities, equipment_fault_probabilities_lower_bound, equipment_fault_probabilities_upper_bound, mode_costs):
+    row_index_to_equipment_name = {index: key for index, key in enumerate(equipment_fault_probabilities)}
+    column_index_to_mode_name = {index: key for index, key in enumerate(mode_costs)}
+
+    best_case = np.zeros([1, len(mode_costs)])
+    worst_case = np.zeros([1, len(mode_costs)])
+    uncertainty_interval = np.zeros([len(equipment_fault_probabilities_lower_bound), len(mode_costs)])
+
+    for row_index, row in enumerate(uncertainty_interval):
+        for column_index, element in enumerate(row):
+            node_id = get_node_id(G, column_index_to_mode_name[column_index])
+            if row_index == 0:
+                best_case[0, column_index] = get_fault_probability(G, node_id, equipment_fault_probabilities_lower_bound)
+                worst_case[0, column_index] = get_fault_probability(G, node_id, equipment_fault_probabilities_upper_bound)
+            # improvement = get_fault_probability(G, node_id, equipment_fault_probabilities_lower_bound) - get_fault_probability(G, node_id, equipment_fault_probabilities)
+            # regression  = get_fault_probability(G, node_id, equipment_fault_probabilities_upper_bound) - get_fault_probability(G, node_id, equipment_fault_probabilities)
+            # Examine best case for this component
+            these_fault_probabilities = equipment_fault_probabilities.copy()
+            these_fault_probabilities[row_index_to_equipment_name[row_index]] = equipment_fault_probabilities_lower_bound[row_index_to_equipment_name[row_index]]
+            best_case_per_component = get_fault_probability(G, node_id, equipment_fault_probabilities_lower_bound)
+            # Examine worst case
+            these_fault_probabilities[row_index_to_equipment_name[row_index]] = equipment_fault_probabilities_upper_bound[row_index_to_equipment_name[row_index]]
+            worst_case_per_component = get_fault_probability(G, node_id, these_fault_probabilities)
+            uncertainty_interval[row_index, column_index] = worst_case_per_component - best_case_per_component
+
+    return uncertainty_interval, best_case, worst_case
+
+def get_best_case_probabilities(G, equipment_fault_probabilities_lower_bound, mode_costs):
+    row_index_to_equipment_name = {index: key for index, key in enumerate(equipment_fault_probabilities_lower_bound)}
+    column_index_to_mode_name = {index: key for index, key in enumerate(mode_costs)}
+
+
+def get_uncertainty_propagation(G, equipment_fault_probabilities, equipment_fault_probabilities_lower_bound, equipment_fault_probabilities_upper_bound, mode_costs):
+    row_index_to_equipment_name = {index: key for index, key in enumerate(equipment_fault_probabilities_lower_bound)}
+    column_index_to_mode_name = {index: key for index, key in enumerate(mode_costs)}
+
+    uncertainty_interval, best_case, worst_case = get_uncertainty_interval(G, equipment_fault_probabilities, equipment_fault_probabilities_lower_bound, equipment_fault_probabilities_upper_bound, mode_costs)
+
+    message = "Uncertainty interval per component\n----------------------------------\n"
+    with pd.option_context('display.max_rows', 100, 'display.max_columns', 20, 'display.width', 200):  # more options can be specified also
+        message += str(pd.DataFrame(uncertainty_interval,
+                                    columns=mode_costs.keys(),
+                                    index=equipment_fault_probabilities_lower_bound.keys()))
+        message += "\n\n"
+
+    for index, column in enumerate(uncertainty_interval.T):
+        message += f"Mode {column_index_to_mode_name[index]}'s uncertainty interval mostly depends on {row_index_to_equipment_name[column.argmax()]}'s uncertainty interval\n"
+    message += "\n"
+
+    for index, row in enumerate(uncertainty_interval):
+        message += f"Component {row_index_to_equipment_name[index]}'s uncertainty interval has most effect on mode {column_index_to_mode_name[row.argmax()]}'s uncertainty interval\n"
+    message += "\n\n"
+
+    message += "Best case\n---------\n"
+    with pd.option_context('display.max_rows', 100, 'display.max_columns', 20, 'display.width', 200):  # more options can be specified also
+        message += str(pd.DataFrame(best_case,
+                                    columns=mode_costs.keys()))
+        message += "\n\n"
+
+    message += "Worst case\n----------\n"
+    with pd.option_context('display.max_rows', 100, 'display.max_columns', 20, 'display.width', 200):  # more options can be specified also
+        message += str(pd.DataFrame(worst_case,
+                                    columns=mode_costs.keys()))
+
+    return message
